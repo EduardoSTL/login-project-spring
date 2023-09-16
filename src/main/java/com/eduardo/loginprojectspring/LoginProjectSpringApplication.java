6a9fae0d72865398a75d4bbed0f61f9e6f1be177
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-09-16 13:45:57 -0600

    Initial commit

---
+++ src/main/java/com/eduardo/loginprojectspring/LoginProjectSpringApplication.java
@@ -0,0 +1,13 @@
+package com.eduardo.loginprojectspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginProjectSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginProjectSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduardo/loginprojectspring/LoginProjectSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardo.loginprojectspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginProjectSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
