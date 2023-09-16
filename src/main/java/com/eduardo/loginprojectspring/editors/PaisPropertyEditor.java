c2d40dc08ecebb18ecac744665f3b1afe326b5d7
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-09-16 15:57:27 -0600

    added: NombreMayusculaEditor.java

---
+++ src/main/java/com/eduardo/loginprojectspring/editors/PaisPropertyEditor.java
@@ -0,0 +1,24 @@
+package com.eduardo.loginprojectspring.editors;
+
+
+import com.eduardo.loginprojectspring.services.PaisService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.beans.PropertyEditorSupport;
+
+@Component
+public class PaisPropertyEditor extends PropertyEditorSupport {
+    @Autowired
+    private PaisService service;
+
+    @Override
+    public void setAsText(String idString) throws IllegalArgumentException{
+        try {
+            Integer id = Integer.parseInt(idString);
+            setValue(service.obtenerPorId(id));
+        } catch (NumberFormatException e){
+            setValue(null);
+        }
+    }
+}
